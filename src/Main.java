940d55abef3f6280457cf8c9ef608f89477ea793
ZhannaEr <eruntalle@gmail.com>
2023-10-11 22:23:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 10_000;
+
+        int bonus = price / 20;
+
+        System.out.println("бонус: " + bonus);
+    }
+}
